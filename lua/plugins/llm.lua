cf3b5c83c36d0c09e9ad5361257892b011b3032b
vanpipy <vanpipy@gmail.com>
2025-06-04 23:44:25 +0800

    fix: remove bracket

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -1,31 +1,30 @@
 return {
-  {
-    "Kurama622/llm.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
-    cmd = { "LLMSessionToggle", "LLMSelectedTextHandler", "LLMAppHandler" },
-    config = function()
-      local tools = require("llm.tools")
+  "Kurama622/llm.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
+  cmd = { "LLMSessionToggle", "LLMSelectedTextHandler", "LLMAppHandler" },
+  config = function()
+    local tools = require("llm.tools")
 
-      require("llm").setup({
-        -- [[ Deepseek ]]
-        url = "https://api.deepseek.com/chat/completions",
-        model = "deepseek-chat",
-        api_type = "openai",
-        max_tokens = 4096,
-        temperature = 0.3,
-        top_p = 0.7,
+    require("llm").setup({
+      -- [[ Deepseek ]]
+      url = "https://api.deepseek.com/chat/completions",
+      model = "deepseek-chat",
+      api_type = "openai",
+      max_tokens = 4096,
+      temperature = 0.3,
+      top_p = 0.7,
 
-        prompt = "You are a helpful chinese assistant.",
+      prompt = "You are a helpful chinese assistant.",
 
-        prefix = {
-          user = { text = "😃 ", hl = "Title" },
-          assistant = { text = "  ", hl = "Added" },
-        },
+      prefix = {
+        user = { text = "😃 ", hl = "Title" },
+        assistant = { text = "  ", hl = "Added" },
+      },
 
-        -- history_path = "/tmp/llm-history",
-        save_session = true,
-        max_history = 15,
-        max_history_name_length = 20,
+      -- history_path = "/tmp/llm-history",
+      save_session = true,
+      max_history = 15,
+      max_history_name_length = 20,
 
         -- stylua: ignore
         keys = {
@@ -56,113 +55,112 @@ return {
           ["JumpToBottom"]      = { mode = "n", key = "G" },
         },
 
-        -- display diff [require by action_handler]
-        display = {
-          diff = {
-            layout = "vertical", -- vertical|horizontal split for default provider
-            opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
-            provider = "mini_diff", -- default|mini_diff
-          },
+      -- display diff [require by action_handler]
+      display = {
+        diff = {
+          layout = "vertical", -- vertical|horizontal split for default provider
+          opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
+          provider = "mini_diff", -- default|mini_diff
         },
-        app_handler = {
-          -- Your AI tools Configuration
-          -- TOOL_NAME = { ... }
-          Completion = {
-            handler = tools.completion_handler,
-            opts = {
-              -------------------------------------------------
-              ---                   ollama
-              -------------------------------------------------
-              -- url = "http://localhost:11434/v1/completions",
-              -- model = "qwen2.5-coder:1.5b",
-              -- api_type = "ollama",
-              ------------------- end ollama ------------------
+      },
+      app_handler = {
+        -- Your AI tools Configuration
+        -- TOOL_NAME = { ... }
+        Completion = {
+          handler = tools.completion_handler,
+          opts = {
+            -------------------------------------------------
+            ---                   ollama
+            -------------------------------------------------
+            -- url = "http://localhost:11434/v1/completions",
+            -- model = "qwen2.5-coder:1.5b",
+            -- api_type = "ollama",
+            ------------------- end ollama ------------------
 
-              -------------------------------------------------
-              ---                  deepseek
-              -------------------------------------------------
-              url = "https://api.deepseek.com/beta/completions",
-              model = "deepseek-chat",
-              api_type = "deepseek",
-              fetch_key = function()
-                return vim.env.LLM_KEY
-              end,
-              ------------------ end deepseek -----------------
+            -------------------------------------------------
+            ---                  deepseek
+            -------------------------------------------------
+            url = "https://api.deepseek.com/beta/completions",
+            model = "deepseek-chat",
+            api_type = "deepseek",
+            fetch_key = function()
+              return vim.env.LLM_KEY
+            end,
+            ------------------ end deepseek -----------------
 
-              -------------------------------------------------
-              ---                 siliconflow
-              -------------------------------------------------
-              -- url = "https://api.siliconflow.cn/v1/completions",
-              -- model = "Qwen/Qwen2.5-Coder-7B-Instruct",
-              -- api_type = "openai",
-              -- fetch_key = function()
-              --   return "your api key"
-              -- end,
-              ------------------ end siliconflow -----------------
+            -------------------------------------------------
+            ---                 siliconflow
+            -------------------------------------------------
+            -- url = "https://api.siliconflow.cn/v1/completions",
+            -- model = "Qwen/Qwen2.5-Coder-7B-Instruct",
+            -- api_type = "openai",
+            -- fetch_key = function()
+            --   return "your api key"
+            -- end,
+            ------------------ end siliconflow -----------------
 
-              -------------------------------------------------
-              ---                  codeium
-              ---    dependency: "Exafunction/codeium.nvim"
-              -------------------------------------------------
-              -- api_type = "codeium",
-              ------------------ end codeium ------------------
+            -------------------------------------------------
+            ---                  codeium
+            ---    dependency: "Exafunction/codeium.nvim"
+            -------------------------------------------------
+            -- api_type = "codeium",
+            ------------------ end codeium ------------------
 
-              n_completions = 3,
-              context_window = 512,
-              max_tokens = 256,
+            n_completions = 3,
+            context_window = 512,
+            max_tokens = 256,
 
-              -- A mapping of filetype to true or false, to enable completion.
-              filetypes = { sh = false },
+            -- A mapping of filetype to true or false, to enable completion.
+            filetypes = { sh = false },
 
-              -- Whether to enable completion of not for filetypes not specifically listed above.
-              default_filetype_enabled = true,
+            -- Whether to enable completion of not for filetypes not specifically listed above.
+            default_filetype_enabled = true,
 
-              auto_trigger = true,
+            auto_trigger = true,
 
-              -- just trigger by { "@", ".", "(", "[", ":", " " } for `style = "nvim-cmp"`
-              only_trigger_by_keywords = true,
+            -- just trigger by { "@", ".", "(", "[", ":", " " } for `style = "nvim-cmp"`
+            only_trigger_by_keywords = true,
 
-              style = "virtual_text", -- nvim-cmp or blink.cmp
+            style = "virtual_text", -- nvim-cmp or blink.cmp
 
-              timeout = 10, -- max request time
+            timeout = 10, -- max request time
 
-              -- only send the request every x milliseconds, use 0 to disable throttle.
-              throttle = 1000,
-              -- debounce the request in x milliseconds, set to 0 to disable debounce
-              debounce = 400,
+            -- only send the request every x milliseconds, use 0 to disable throttle.
+            throttle = 1000,
+            -- debounce the request in x milliseconds, set to 0 to disable debounce
+            debounce = 400,
 
-              --------------------------------
-              ---   just for virtual_text
-              --------------------------------
-              keymap = {
-                virtual_text = {
-                  accept = {
-                    mode = "i",
-                    keys = "<A-a>",
-                  },
-                  next = {
-                    mode = "i",
-                    keys = "<A-n>",
-                  },
-                  prev = {
-                    mode = "i",
-                    keys = "<A-p>",
-                  },
-                  toggle = {
-                    mode = "n",
-                    keys = "<leader>cp",
-                  },
+            --------------------------------
+            ---   just for virtual_text
+            --------------------------------
+            keymap = {
+              virtual_text = {
+                accept = {
+                  mode = "i",
+                  keys = "<A-a>",
+                },
+                next = {
+                  mode = "i",
+                  keys = "<A-n>",
+                },
+                prev = {
+                  mode = "i",
+                  keys = "<A-p>",
+                },
+                toggle = {
+                  mode = "n",
+                  keys = "<leader>cp",
                 },
               },
             },
           },
         },
-      })
-    end,
-    keys = {
-      { "<leader>ac", mode = "n", "<cmd>LLMSessionToggle<cr>" },
-      { "<leader>ae", mode = "v", "<cmd>LLMSelectedTextHandler please explain the codes<cr>" },
-      { "<leader>ts", mode = "x", "<cmd>LLMSelectedTextHandler en to cn<cr>" },
-    },
+      },
+    })
+  end,
+  keys = {
+    { "<leader>ac", mode = "n", "<cmd>LLMSessionToggle<cr>" },
+    { "<leader>ae", mode = "v", "<cmd>LLMSelectedTextHandler please explain the codes<cr>" },
+    { "<leader>ts", mode = "x", "<cmd>LLMSelectedTextHandler en to cn<cr>" },
   },
 }
